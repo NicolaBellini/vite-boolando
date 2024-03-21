dc38e01d43e5816cec9d53c5e15e8c69e6619054
NicolaBellini <bellininicola4366@gmail.com>
2024-03-21 14:38:52 +0100

    scufolding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Header from "./components/Header.vue";
+// ???? mi dice che il file è gia stato incluso
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <Header />
+  <Main />
+  <Footer />
+</template>
+
+<style lang="scss"></style>
